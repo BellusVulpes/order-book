3afd62cfaf832880098cfc82681a525a0e88b2d4
christopher boucher <chrismichb@yahoo.com>
2021-05-01 20:53:00 -0400

    Refactored push to empty book

--- orderBook.js
+++ orderBook.js
@@ -1,5 +1,11 @@
 const reconcileOrder = (existingBook, incomingOrder) => {
-  
+  let updatedBook = []
+
+  if (existingBook.length === 0) {
+    updatedBook.push(incomingOrder)
+
+    return updatedBook
+  }
 }
 
 module.exports = reconcileOrder
