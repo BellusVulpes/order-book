0e751aa22a41baaeea3ac6ff836c92e0050adff7
christopher boucher <chrismichb@yahoo.com>
2021-05-01 21:54:15 -0400

    created if statements with blank functions

--- orderBook.js
+++ orderBook.js
@@ -6,6 +6,27 @@ const reconcileOrder = (existingBook, incomingOrder) => {
 
     return updatedBook
   }
+
+  existingBook.array.forEach((curOrder, index) => {
+    if (orderType(existingBook, incomingOrder) && orderPrice(existingBook, incomingOrder)) {
+      const quantity = findQuantity(existingBook, incomingOrder)
+
+      existingBook.quantity -= quantity
+      incomingOrder.quantity -= quantity
+    }
+
+    if (existingBook.quantity > 0) {
+      updatedBook.push(existingBook)
+    }
+
+    existingBook.splice(index, 1)
+  });
+
+  if (incomingOrder.quantity > 0) {
+    updatedBook.push(incomingOrder)
+  }
+
+  return updatedBook
 }
 
 module.exports = reconcileOrder
